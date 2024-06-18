a4ca1448982ad3da3751f245e115b8b8c90009b7
geka001 <geethanjali.k@gmail.com>
2024-06-18 13:15:18 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a Program Manager driving quality initiatives with over 11 years of experience, including as a QA Team Lead. Known for enhancing team effectiveness and productivity through strategic test initiatives and a strong sense of ownership. Expertise in inspiring, mentoring, leading, and managing teams towards excellence. Passionate about continuous learning and women empowerment, dedicated to fostering supportive and inclusive work environments. Equipped with the vision and skills to lead successful program management efforts, ensuring seamless execution and impactful outcomes.`;
+
+export const ABOUT_TEXT = `Hardworking, accountable, dedicated, and versatile, I am a seasoned Program Manager with over 11 years of experience driving quality initiatives and leading diverse teams. I excel in utilizing technologies such as Java, Selenium, TestNG, RestAssured, and AWS to deliver high-quality software solutions. With certifications in Agile, Scrum, PSM1, and CSPO, I am skilled in developing strategic test plans, managing projects, and ensuring timely execution. Passionate about fostering inclusive work environments and empowering women in tech, I am also enthusiastic about continuous learning and personal growth. Outside of work, I enjoy reading, listening to music, and walking.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2024 - Present",
+    role: "Program Manager",
+    company: "Contentstack",
+    description: `Driving quality intiatives. Working on enhancing team effectiveness and productivity through strategic test initiatives and a strong sense of ownership`,
+    technologies: ["Java/Selenium", "Jira", "Github", "Whimsical"],
+  },
+  {
+    year: "2019 - 2024",
+    role: "Team Lead",
+    company: "SoftwareAG",
+    description: `Leading a team of 10 QA engineers to ensure the delivery of high-quality software within tight deadlines. Left shift tests execution, 80% reduction in test execution time, conducting team training on new testing technologies, process improvements, reviews,resulting in a 40% increase in team productivity`,
+    technologies: ["Java/Selnium/Cucumber", "AWS", "GitHub", "Azure"],
+  },
+  {
+    year: "2010 - 2012",
+    role: "QA Specialist",
+    company: "SoftwareAG",
+    description: `Managing the sustaining promotion activities for various releases like 7x, 8x . Mentoring new team members, interns, contractors in sustaining process, automation framework, automation coding guidelines`,
+    technologies: ["Java", "Jenkins", "Rest Assured", "Selenium"],
+  },
+  {
+    year: "2007 - 2010",
+    role: "Senior Quality Assurance Engineer",
+    company: "Oracle",
+    description: `Certified the quality of OBIEE Segmentation Server by writing test plans, executing tests in various platforms, WebServices Automation. Involved in FDD Reviews,test design, test scripts and test data, test execution, QTP automation in CRM Fusion Marketing, Siebel PRM`,
+    technologies: ["QTP", "QualityCenter", "Fusion", "Siebel PRM"],
+  },
+  {
+    year: "2004 - 2005",
+    role: "Software Quality Analyst",
+    company: "Intel",
+    description: `Co-ordination with stakeholders to ensure completion and timely delivery. Project tracking, escalations, test plan creation/execution/bug tracking, test harness development. Requirement analysis,design reviews,testdata creation,execution OCOT`,
+    technologies: ["C#", "Zephyer", "OCOT", ".NET"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "#1F 001, Akme Harmony, Bellandur, Bengaluru ",
+  phoneNo: "+91 96295 33225 ",
+  email: "geethanjali.k@gmail.com",
+};
