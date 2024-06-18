f6dcb8497ca419bc4bb2025e095367f293a0d499
geka001 <geethanjali.k@gmail.com>
2024-06-18 15:23:01 +0530

    fixed color issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,8 +31,8 @@ const Hero = () => {
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500
-              to purple-500 bg-clip-text text-4xl tracking-tight text-transparent">   
-            Program Manager - Contentstack  ...
+              to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">   
+            Program Manager - Contentstack
             </motion.span>
             <motion.p
               variants={container(1)}
